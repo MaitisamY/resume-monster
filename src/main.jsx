0975f8d46bd69074f46c5f6fbd1a60ed194079e4
Muhammad Aitisam Yaseen <m.aitisamyaseen@gmail.com>
2024-02-04 18:02:33 +0500

    Complete resume form section

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './dev-defined.css'
-import './components/template.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
